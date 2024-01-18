887afbf495d3190a49a9e1d53727a3e3eccd37fc
BitBard95 <156848625+BitBard95@users.noreply.github.com>
2024-01-18 15:00:56 -0500

    Update random.c
    Lab 01

--- random.c
+++ random.c
@@ -1,1 +1,7 @@
-adhalh
+// Function to generate a random uppercase letter
+char randchar() {
+    int alpha = rand() % 26;
+    char array[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','\0'};
+    char rand_char = array[alpha];
+    return rand_char;
+}
